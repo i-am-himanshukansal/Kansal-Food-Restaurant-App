4e4891e988b0a730e448532b29d458a4f6a89b5c
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2026-01-04 23:28:33 +0530

    correction

--- frontend/pages/NotFound.jsx
+++ frontend/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
       <div className="container">
         <img src="/notFound.svg" alt="Page not found" />
         <h1>Looks like you’re lost.</h1>
-        <p>We can’t seem to find the page you’re looking for.</p>
+        <p>We can’t seem to find the page you are looking for.</p>
         <Link to="/">
           Back to home!<span><HiOutlineArrowRight /></span>
         </Link>
